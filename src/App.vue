c79e3d1f19d8c8c1ce43a50b589989efc22ce363
mfsv <m.sheraliyev1011@gmail.com>
2024-08-04 14:28:39 +0500

    initialize

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+    <div class="container mx-auto">
+        <div class="h-dvh w-full flex justify-center items-center">
+            <h1 class="text-4xl font-semibold text-teal-700">Vite, Vue 3 CompositionAPI, TailwindCSS</h1>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
